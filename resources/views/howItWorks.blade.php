0703c865a146ef51b2f938ebcce82c6f2dd4b2fa
AtikCSE113 <atikulislamsa@gmail.com>
2023-05-13 01:03:26 +0600

    first commit

---
+++ resources/views/howItWorks.blade.php
@@ -0,0 +1,55 @@
+@extends('layout')
+
+@section('hero')
+    <!-- ======= Hero Section ======= -->
+  {{-- <section id="hero1" class="d-flex align-items-center terms">
+
+    <div class="container">
+      <div class="row">
+
+
+      </div>
+    </div>
+
+
+  </section><!-- End Hero --> --}}
+@endsection
+
+@section('main')
+  <main id="main" style="margin-top: 100px;">
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>How it works</h2>
+          <p>We've made it easy for you to book. Here's how.</p>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <p>
+               <b>1. Search for your trip</b>
+              
+            </p>
+
+            <p>
+               <b>2. Choose your trip and pay for your ticket</b>
+              
+            </p>
+
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+               <b>3. You're all set! </b>
+               
+            </p>
+           <!-- <a href="#" class="btn-learn-more">Learn More</a> -->
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+  </main>
+@endsection
